3a617c8ac50e91ef4e19e5ac3e4aa0307b652755
Luis Alvarenga <luis.amayaalvarenga@gmail.com>
2022-02-09 18:11:05 -0600

    Agregar archivos de proyecto.

---
+++ AppTestOrdering/AppTestOrdering/Views/Controls/MonkeyListTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTestOrdering.Views.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonkeyListTemplate : Frame
+    {
+        public MonkeyListTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppTestOrdering/AppTestOrdering/Views/SelectItems.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTestOrdering.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectItems : ContentPage
+    {
+        public SelectItems()
+        {
+            InitializeComponent();
+            BindingContext = ViewModels.ItemsVM.Current;
+        }
+    }
+}
